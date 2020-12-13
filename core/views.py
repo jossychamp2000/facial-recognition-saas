1e3d9b3170e53e60249155712cde11c09c5cfc92
janejane <janetjanedchic@yahoo.com>
2020-12-12 22:45:19 -0500

    stripe ID updated at views.py&base.py

--- core/views.py
+++ core/views.py
@@ -23,7 +23,7 @@ import stripe
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
 User = get_user_model()
-SRIPE_PLAN_ID = 'price_1HxkcJG2pgXqIT36wqihydte'
+SRIPE_PLAN_ID = 'price_1Hopp3G2pgXqIT362iXgbYyE'
 
 
 def get_user_from_token(request):
--- home/settings/base.py
+++ home/settings/base.py
@@ -98,4 +98,4 @@ ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 # Stripe plan ID
 
-STRIPE_PLAN_ID = 'price_1HxkcJG2pgXqIT36wqihydte'
+STRIPE_PLAN_ID = 'price_1Hopp3G2pgXqIT362iXgbYyE'
